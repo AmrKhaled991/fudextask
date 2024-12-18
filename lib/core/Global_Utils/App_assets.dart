b768fcfa0f9f98d30cce7417905c4cc034f16f25
amr <amrkhaled033@gmail.com>
2024-12-18 14:26:12 +0200

    set AppBase

---
+++ lib/core/Global_Utils/App_assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesEllipse
+  /// assets/images/Ellipse.svg
+  static const String imagesEllipse = "assets/images/Ellipse.svg";
+
+  /// Assets for imagesTrash
+  /// assets/images/trash.svg
+  static const String imagesTrash = "assets/images/trash.svg";
+}
+
---
+++ lib/core/constent.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color PRIMARY = Color(0xff28A6FD);
+const Color SECONDRY = Color(0xff4EB7F2);
